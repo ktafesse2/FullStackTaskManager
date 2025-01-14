a26a676d669ced492fb4615c247f3721e0ab28b0
Kaleb <kaleb.e.tafesse@vanderbilt.edu>
2025-01-13 21:24:01 -0600

    Initial setup: Full-Stack Structure

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function Tasks() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Task Manager</Text>
+    </View>
+  );
+}
