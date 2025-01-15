8ab06df536e9521199a72cee00f89bc1c5a4b96e
Kaleb <kaleb.e.tafesse@vanderbilt.edu>
2025-01-14 22:45:41 -0600

    Formatting Update

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,5 +1,4 @@
 import { View, Text } from 'react-native';
-
 export default function Profile() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
--- frontend/app/(tabs)/tasks.tsx
+++ frontend/app/(tabs)/tasks.tsx
@@ -1,5 +1,4 @@
 import { View, Text } from 'react-native';
-
 export default function Tasks() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
